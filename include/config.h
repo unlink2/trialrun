3361c288a17924533d900921ddcb7257036975b8
Lukas Krickl <lukas@krickl.dev>
2022-09-19 20:08:55 +0200

    Added cli arguments

--- include/config.h
+++ include/config.h
@@ -1,7 +1,9 @@
 #ifndef CONFIG_H_
 #define CONFIG_H_
 
+#include "error.h"
 #include "types.h"
+#include <stdio.h>
 
 #define DEFAULT_PATH_IN "-"
 #define DEFAULT_PATH_OUT "-"
@@ -23,5 +25,12 @@ void cfg_init(Config *cfg);
 // this macro obtains the config object and
 // creates it in the local scope
 #define cfg() Config *cfg = &global_cfg;
+
+// Re-open stdout to output path if path is not -
+FILE *open_output(Config *cfg);
+
+// run all tests if path is a directory
+// or a single test if path is a file
+Errors run_tests(Config *cfg);
 
 #endif
--- src/config.c
+++ src/config.c
@@ -1,5 +1,7 @@
 #include "config.h"
 #include "log.h"
+#include <stdio.h>
+#include <string.h>
 
 Config global_cfg;
 
@@ -8,3 +10,12 @@ void cfg_init(Config *cfg) {
   cfg->in_path = DEFAULT_PATH_IN;
   cfg->out_path = DEFAULT_PATH_OUT;
 }
+
+FILE *open_output(Config *cfg) {
+  if (strncmp(cfg->out_path, DEFAULT_PATH_OUT, strlen(DEFAULT_PATH_OUT)) == 0) {
+    return stdout;
+  }
+  return freopen(cfg->out_path, "we", stdout);
+}
+
+Errors run_tests(Config *cfg) { return OK; }
